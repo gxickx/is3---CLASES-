011381edaf4b40cebc11b71bc94e2fddfe52f665
gxickx <mgrokoe@gmail.com>
2025-04-23 16:35:55 -0300

    Agregué el Import java util *
    Signed-off-by: gxickx <mgrokoe@gmail.com>

--- src/is3Versionado.java
+++ src/is3Versionado.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class is3Versionado {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
